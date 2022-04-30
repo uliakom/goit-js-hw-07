4c26865ec5eddafc34c870208af9448de13f7a02
Uliana Komarynska <uliakom@gmail.com>
2022-04-30 19:06:00 +0300

    2 tasks made

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,57 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', onImageClick);
+
+function createGalleryMarkup(imgElements) {
+    return imgElements
+        .map(({ preview, original, description }) => {
+            return  `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt='${description}'
+    />
+  </a>
+</div>`;  
+    }
+    ).join(''); 
+};
+
+function onImageClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    else {
+        event.target.src = event.target.dataset.source;
+        const instance = basicLightbox.create(`
+    <div class="modal">
+    <img
+      src=${event.target.src}
+      alt='${event.target.alt}'
+    />
+    </div>
+`,{ onShow: () => {
+            window.addEventListener('keydown', onEscKeyPress )
+            },
+        })
+      
+      function onEscKeyPress(event) {
+  if (event.code === 'Escape') {
+    instance.close();
+     window.removeEventListener('keydown', onEscKeyPress);
+  }
+      };
+      
+instance.show()
+    }
+};
+
